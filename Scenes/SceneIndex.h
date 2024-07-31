816240059edd920fb49b72c8f93f08eaf953bdb1
David Berger <david.berger@tum.de>
2024-07-31 21:27:37 +0200

    Remove unused files

--- Scenes/SceneIndex.h
+++ Scenes/SceneIndex.h
@@ -4,8 +4,8 @@
 #include <functional>
 
 #include "Scene1.h"
-#include "Scene2.h"
-#include "Scene3.h"
+// #include "Scene2.h"
+// #include "Scene3.h"
 // #include "Scene4.h"
 
 using SceneCreator = std::function<std::unique_ptr<Scene>()>;
@@ -13,10 +13,10 @@ using SceneCreator = std::function<std::unique_ptr<Scene>()>;
 std::map<std::string, SceneCreator> scenesCreators = {
     {"Scene1", []()
      { return std::make_unique<Scene1>(); }},
-    {"Scene2", []()
-     { return std::make_unique<Scene2>(); }},
-    {"Scene3", []()
-     { return std::make_unique<Scene3>(); }},
+    // {"Scene2", []()
+    //  { return std::make_unique<Scene2>(); }},
+    // {"Scene3", []()
+    //  { return std::make_unique<Scene3>(); }},
     // {"Scene4", []()
     //  { return std::make_unique<Scene4>(); }},
     // add more Scene types here
