e013a9b40a51a21ee2c10e04f51bb248aa8c5693
David Berger <david.berger@tum.de>
2024-07-31 16:03:50 +0200

    Rotate one rb for cleaner collision

--- Scenes/Scene3.cpp
+++ Scenes/Scene3.cpp
@@ -9,6 +9,7 @@ void Scene3::init()
     rigidbodies.push_back(rb1);
 
     Rigidbody rb2(vec3(1, 1, 0), vec3(1, 2, 1));
+    rb2.rotation = glm::quat(glm::vec3(glm::radians(0.0f), glm::radians(45.0f), glm::radians(10.0f)));
     rb2.velocity = vec3(-1.0f, 0, 0);
     rigidbodies.push_back(rb2);
 }
