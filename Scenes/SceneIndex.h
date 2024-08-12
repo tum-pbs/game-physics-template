27264abff3e6b5b938b461bdb6723e6eeda32203
David Berger <david.berger@tum.de>
2024-08-12 13:10:06 +0200

    Add Scene 2

--- Scenes/SceneIndex.h
+++ Scenes/SceneIndex.h
@@ -4,7 +4,7 @@
 #include <functional>
 
 #include "Scene1.h"
-// #include "Scene2.h"
+#include "Scene2.h"
 // #include "Scene3.h"
 // #include "Scene4.h"
 
@@ -13,8 +13,8 @@ using SceneCreator = std::function<std::unique_ptr<Scene>()>;
 std::map<std::string, SceneCreator> scenesCreators = {
     {"Scene1", []()
      { return std::make_unique<Scene1>(); }},
-    // {"Scene2", []()
-    //  { return std::make_unique<Scene2>(); }},
+    {"Scene2", []()
+     { return std::make_unique<Scene2>(); }},
     // {"Scene3", []()
     //  { return std::make_unique<Scene3>(); }},
     // {"Scene4", []()
