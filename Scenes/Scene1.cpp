a3d06f4b1e3172e9fa0da313a30d59f278ab87c4
David Berger <david.berger@tum.de>
2024-08-14 14:33:27 +0200

    Make edge persistent if drawn on

--- Scenes/Scene1.cpp
+++ Scenes/Scene1.cpp
@@ -20,5 +20,15 @@ void Scene1::diffuseExplicit(float dt)
             newGrid(y, x) = grid(y, x) + dt * alpha * (plusX + minusX + plusY + minusY - 4 * grid(y, x));
         }
     }
+    for (int x = 0; x < grid.width; x++)
+    {
+        newGrid(0, x) = grid(0, x);
+        newGrid(grid.height - 1, x) = grid(grid.height - 1, x);
+    }
+    for (int y = 0; y < grid.height; y++)
+    {
+        newGrid(y, 0) = grid(y, 0);
+        newGrid(y, grid.width - 1) = grid(y, grid.width - 1);
+    }
     grid = newGrid;
 }
