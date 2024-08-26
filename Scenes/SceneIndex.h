013ac028abeb75a1c5787ea8240606b0925083ba
David Berger <david.berger@tum.de>
2024-08-26 11:12:41 +0200

    Use more descriptive scene titles

--- Scenes/SceneIndex.h
+++ Scenes/SceneIndex.h
@@ -3,19 +3,13 @@
 
 #include "Scene1.h"
 #include "Scene2.h"
-// #include "Scene3.h"
-// #include "Scene4.h"
 
 using SceneCreator = std::function<std::unique_ptr<Scene>()>;
 
 std::map<std::string, SceneCreator> scenesCreators = {
-    {"Scene1", []()
+    {"Explicit Euler", []()
      { return std::make_unique<Scene1>(); }},
-    {"Scene2", []()
+    {"Implicit Euler", []()
      { return std::make_unique<Scene2>(); }},
-    // {"Scene3", []()
-    //  { return std::make_unique<Scene3>(); }},
-    // {"Scene4", []()
-    //  { return std::make_unique<Scene4>(); }},
     // add more Scene types here
 };
