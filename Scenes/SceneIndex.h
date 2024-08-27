aa1e3cbed30b64f1eae1bcc1791d975b3f56a857
David Berger <david.berger@tum.de>
2024-08-27 19:40:58 +0200

    Reduce code duplication

--- Scenes/SceneIndex.h
+++ Scenes/SceneIndex.h
@@ -5,8 +5,14 @@
 
 using SceneCreator = std::function<std::unique_ptr<Scene>()>;
 
+template <typename T>
+SceneCreator creator()
+{
+    return []()
+    { return std::make_unique<T>(); };
+}
+
 std::map<std::string, SceneCreator> scenesCreators = {
-    {"Demo Scene", []()
-     { return std::make_unique<Scene1>(); }},
+    {"Demo Scene", creator<Scene1>()},
     // add more Scene types here
 };
