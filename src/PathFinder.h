d87472a296dbcf7079fd25a36713cd5ff67c731f
Rene <rene.winchenbach@gmail.com>
2024-11-21 16:59:33 +0100

    Add guide and add portable version for pathfinder that doesnt look in non portable paths (the default behavior)

--- src/PathFinder.h
+++ src/PathFinder.h
@@ -5,7 +5,7 @@
 #include <string>
 
 std::filesystem::path expand(std::filesystem::path in);
-std::filesystem::path resolveFile(std::string fileName, std::vector<std::string> search_paths = {});
+std::filesystem::path resolveFile(std::string fileName, std::vector<std::string> search_paths = {}, bool portable = false);
 
 extern std::filesystem::path workingDirectory;
 extern std::filesystem::path binaryDirectory;
